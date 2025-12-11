9dd0524c66e107173b887e8102fb5d3c3ba80d34
Ruben <555441@student.saxion.nl>
2025-12-11 11:05:49 +0100

    refactor: Remove unused saveResultsPlugin import from Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import saveResultsPlugin from './vite-plugin-save-results.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
